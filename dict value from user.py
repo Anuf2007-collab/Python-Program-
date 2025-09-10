ceb80f97f648341a3c29249aa2006be9def5be95
Anuf2007-collab <mohammedanuf2007@gmail.com>
2025-09-10 17:30:32 +0530

    Add files via upload

---
+++ dict value from user.py
@@ -0,0 +1,10 @@
+d={}
+print(d)
+n=int(input("enter the no. of elements"))
+for i in range(n):
+    key=input("enter the key of element")
+    value=input("enter the value of element")
+    d[key]=value
+print(d)
+
+
